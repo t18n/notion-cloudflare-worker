b486b37cdde94997bdeb42f619b114c8986fa6ca
Turbo Thinh <turbothinh@gmail.com>
2020-11-26 00:54:21 +0100

    Add slugs

--- src/custom/slugs.ts
+++ src/custom/slugs.ts
@@ -2,7 +2,31 @@
   * Map Slug to notion Page id
   */
 export const prettifySlugs: Record<string, string> = {
+  // Pages
   '': '123c6dcd25474991a03545265c290beb',
-  'about': '370a474c7d0b42aa8f7fbbd29512622c',
-  'life': '93e4fa446ca24207b33e70c9f6d89ec3'
+  'life': 'fe1790b5eb2843ed803b56a7d90e298d',
+  'tech': '7224be26a58a4dcd946781f255b0bd81',
+  'quotes': 'ccbdd387723f49c983486cff4935627c',
+  'contact': 'f037581f0242455bbba6c382ecf6f463',
+  'other-areas': 'a50cb27957b742bf9d1256f4d84837df',
+  'navigation': '9f7ba95e8e6c451ba396a5c41596dad7',
+  'about': '9a3936acdf444bf999d9752e6ad4a5dd',
+  'movies': '0b8a94b3d4f04d8abbe6201983ddbf38',
+  'books': '0cc83c4fb0754c718132387163c4c34c',
+  'projects': 'a8d584533d1d4f178d3e99f385d5b2de',
+  'games': 'a696183be20c49c891e8db36a73176f0',
+  'fitness': '6d44c0c52f844c37aad12e26568123ed',
+  'calm': 'a50cb27957b742bf9d1256f4d84837df',
+  'entrepreneurship': 'a50cb27957b742bf9d1256f4d84837df',
+  'nutrition': 'b2ba07f170e448f09ccb7fa99c980006',
+  'english': 'fc5d50ed99ac47ada5f3d7ab941ed8b6',
+
+  // Life Blogs
+  'lam-ban-tam-quoc': 'bdf7f816637c4671bf1143a612b948bf',
+  'dan-ao-cho-anh': 'd652e803cda1464a8e81333f5e1941ee',
+  'be-doi': 'b4544681bacb4f4eb7d5c46abb063b37',
+  'chuyen-tinh-cay-va-la': 'b6d9b61405c54b74940da5bf4b0bbd35',
+
+  // Tech blogs
+  'convert-svg-to-png': '5bda89c1fe65452dacd6924c65a82beb'
 };
